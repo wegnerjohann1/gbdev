b54778b1324471113028159e09d4417f4e718ef9
Johann Wegner <wegnerjohann1@gmail.com>
2024-07-13 12:10:58 +0200

    implemented ram read/write

--- lib/ram.c
+++ lib/ram.c
@@ -6,13 +6,50 @@ u8 wram_read(u16 address)
 {
     address -= 0xC000;
 
-    if (address >= 2000)
+    if (address >= 0x2000)
     {
-        printf("INVALID WRAM ADDR %04X\n", address + 2000);
+        printf("INVALID WRAM ADDR %04X\n", address + 0x2000);
         exit(MEMORY_OUT_OF_RANGE);
     }
-}
-void wram_write(u16 address, u8 value);
 
-u8 hram_read(u16 address);
-void hram_write(u16 address, u8 value);
+    return ctx.wram[address];
+}
+
+void wram_write(u16 address, u8 value)
+{
+    address -= 0xC000;
+    
+    if (address >= 0x2000)
+    {
+        printf("INVALID WRAM ADDR %04X\n", address + 0x2000);
+        exit(MEMORY_OUT_OF_RANGE);
+    }
+
+    ctx.wram[address] = value;
+}
+
+u8 hram_read(u16 address)
+{
+    address -= 0xFF80;
+
+    if (address >= 80)
+    {
+        printf("INVALID HRAM ADDR %04X\n", address + 0xFF80);
+        exit(MEMORY_OUT_OF_RANGE);
+    }
+
+    return ctx.hram[address];
+}
+
+void hram_write(u16 address, u8 value)
+{
+    address -= 0xFF80;
+
+    if (address >= 80)
+    {
+        printf("INVALID HRAM ADDR %04X\n", address + 0xFF80);
+        exit(MEMORY_OUT_OF_RANGE);
+    }
+
+    ctx.hram[address] = value;
+}
