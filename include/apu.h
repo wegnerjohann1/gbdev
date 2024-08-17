fbabc5bb8f68d2e550799ce9003b5baa1bd15e85
Johann Wegner <wegnerjohann1@gmail.com>
2024-08-17 15:42:05 +0200

    created apu.h

---
+++ include/apu.h
@@ -0,0 +1,15 @@
+#include <defs.h>
+
+typedef struct
+{
+    /* data */
+} channel;
+
+typedef struct
+{
+    channel square1;
+    channel square2;
+    channel wave;
+    channel noise;
+} apu_context;
+
